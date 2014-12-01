a9d02d7c096ddf70832e3838c2d48f88fba588cb
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2014-12-01 09:04:58 -0500

    Return full user after update

--- Handler/Result.hs
+++ Handler/Result.hs
@@ -7,6 +7,10 @@ putResultR :: TribeId -> EventId -> ResultId -> Handler ()
 putResultR _ _ rid = do
   r <- requireJsonBody :: Handler Result
   requireUserSession $ resultUser r
-  runDB $ update rid [ResultReps =. resultReps r, ResultTime =. resultTime r, ResultPr =. resultPr r]
+  runDB $ update rid
+    [ ResultReps  =. resultReps r
+    , ResultTime  =. resultTime r
+    , ResultPr    =. resultPr r
+    ]
   sendResponseStatus status200 ()
 
--- Handler/User.hs
+++ Handler/User.hs
@@ -1,6 +1,14 @@
 module Handler.User where
 
 import Import
+import Helpers.Request
 
-putUserR :: UserId -> Handler ()
-putUserR _ = sendResponseStatus status200 ()
+putUserR :: UserId -> Handler Value
+putUserR uid = do
+  requireUserSession uid
+  u <- requireJsonBody :: Handler User
+  runDB $ update uid
+    [ UserTribe         =. userTribe u
+    , UserAcceptedTerms =. userAcceptedTerms u
+    ]
+  return $ object ["user" .= Entity uid u]
