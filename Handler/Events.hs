6c372aea26073e70b6321bbccecd9c7f08297db1
Tony DiPasquale <tony@thoughtbot.com>
2015-03-13 16:15:46 -0400

    Add event get by start and end date

--- Handler/Events.hs
+++ Handler/Events.hs
@@ -1,22 +1,34 @@
 module Handler.Events where
 
-import Import hiding ((==.))
+import Import hiding ((==.), (<=.), (>=.), parseTime)
 import Database.Esqueleto hiding (Value)
 import Helpers.Request
 import Type.EventModel
+import Data.Text (unpack)
+import Data.Time (getCurrentTime, parseTime, UTCTime)
+import System.Locale (defaultTimeLocale)
 
 getEventsR :: TribeId -> Handler Value
 getEventsR tid = do
   requireSession
-  events <- runDB findEvents :: Handler [EventModel]
+
+  startTimeString <- lookupGetParam "start_date"
+  endTimeString <- lookupGetParam "end_date"
+  defaultTime <- lift getCurrentTime
+  let startDate = maybe defaultTime id $ (unpack <$> startTimeString) >>= dateFromString
+  let endDate = maybe defaultTime id $ (unpack <$> endTimeString) >>= dateFromString
+
+  events <- runDB $ findEvents startDate endDate :: Handler [EventModel]
   return $ object ["events" .= events]
   where
-    findEvents =
+    findEvents stime etime = do
       select $ 
         from $ \(event `LeftOuterJoin` workout `LeftOuterJoin` location) -> do
         on $ event ^. EventLocation ==. location ?. LocationId
         on $ event ^. EventWorkout ==. workout ?. WorkoutId
         where_ (event ^. EventTribe ==. val tid)
+        where_ (event ^. EventDate >=. val stime)
+        where_ (event ^. EventDate <=. val etime)
         let vc = sub_select $
                  from $ \v -> do
                  where_ (v ^. VerbalEvent ==. event ^. EventId)
@@ -34,3 +46,5 @@ postEventsR tid = do
   _     <- runDB $ insert event
   sendResponseStatus status201 ()
 
+dateFromString :: String -> Maybe UTCTime
+dateFromString = parseTime defaultTimeLocale "%Y-%m-%d"
