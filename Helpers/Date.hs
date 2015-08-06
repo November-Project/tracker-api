49906f610f6f3b923ba9cf1573c08c807e5fdc5e
Tony DiPasquale <tony@thoughtbot.com>
2015-08-06 18:21:47 -0400

    Allow military and 12 hour time formats

--- Helpers/Date.hs
+++ Helpers/Date.hs
@@ -2,10 +2,13 @@ module Helpers.Date where
 
 import ClassyPrelude.Yesod
 import Data.Time (parseTimeM, TimeOfDay)
+import Control.Applicative (Alternative)
 
 parseGregorianDate :: Monad m => String -> m Day
 parseGregorianDate = parseTimeM True defaultTimeLocale "%F"
 
-parseTimeOfDay :: Monad m => String -> m TimeOfDay
-parseTimeOfDay = parseTimeM True defaultTimeLocale "%R"
+parseTimeOfDay :: (Monad m, Alternative m) => String -> m TimeOfDay
+parseTimeOfDay s = parse "%k:%M" s
+                   <|> parse "%l:%M %p" s
+                   where parse = parseTimeM True defaultTimeLocale
 
