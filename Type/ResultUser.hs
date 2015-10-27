be029aa350527ddcf0eb848b5b328f750c9cc1f5
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2015-10-26 22:31:05 -0700

    Add photo url to result json

--- Type/ResultUser.hs
+++ Type/ResultUser.hs
@@ -7,12 +7,13 @@ type ResultUser = (Entity Result, Entity User)
 
 instance ToJSON ResultUser where
   toJSON (Entity rid r, Entity uid u) = object
-    [ "id"        .= rid
-    , "user_id"   .= uid
-    , "user_name" .= userName u
-    , "event_id"  .= resultEvent r
-    , "event_time".= resultEventTime r
-    , "reps"      .= resultReps r
-    , "time"      .= resultTime r
-    , "pr"        .= resultPr r
+    [ "id"              .= rid
+    , "user_id"         .= uid
+    , "user_name"       .= userName u
+    , "user_photo_url"  .= userPhotoUrl u
+    , "event_id"        .= resultEvent r
+    , "event_time"      .= resultEventTime r
+    , "reps"            .= resultReps r
+    , "time"            .= resultTime r
+    , "pr"              .= resultPr r
     ]
