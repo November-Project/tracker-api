0af599b0adde4101a122bbef574c1f3a83d0cdaf
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2016-01-21 17:37:38 -0800

    Fixup query to fix facebook id issues

--- fix_facebook_ids.sql
+++ fix_facebook_ids.sql
@@ -4,10 +4,13 @@ DO $$
         keepID BIGINT;
         oldIDs users.id%type;
 BEGIN
-    FOR fid IN (select facebook_id as fid from users group by facebook_id having count(*)>1)
+    FOR fid IN (select facebook_id as fid from users where facebook_id is not null group by facebook_id having count(*)>1)
     LOOP
+        RAISE NOTICE 'Processing %', fid;
         keepID := (select MAX(id) from users where facebook_id = fid);
         select id into oldIDs from users where facebook_id = fid EXCEPT select keepID;
+        RAISE NOTICE 'Keeping %', keepID;
+        RAISE NOTICE 'Removing %', oldIDs;
 
         BEGIN
             update verbals set "user" = keepID where "user" in (select oldIDs);
@@ -25,13 +28,6 @@ BEGIN
 
         delete from users where id in (select oldIDs);
     END LOOP;
-
-    IF NOT EXISTS
-        (select constraint_name
-          from information_schema.constraint_column_usage
-          where table_name = 'users' and constraint_name = 'unique_user_facebook_id') THEN
-        ALTER TABLE "users" ADD CONSTRAINT "unique_user_facebook_id" UNIQUE("facebook_id");
-    END IF;
     RETURN;
 END;
 $$ LANGUAGE plpgsql;
