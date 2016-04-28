34dc85e8d266adc6c4e3dc32d6d747ff616dcdae
Tony DiPasquale <tony@thoughtbot.com>
2016-04-28 15:53:45 -0700

    Add some indices to speed up queries

---
+++ database/indicies.sql
@@ -0,0 +1,8 @@
+CREATE INDEX ON "users" ("name");
+CREATE INDEX ON "users" ("email");
+CREATE INDEX ON "verbals" ("user");
+CREATE INDEX ON "verbals" ("event");
+CREATE INDEX ON "results" ("user");
+CREATE INDEX ON "results" ("event");
+CREATE INDEX ON "events" ("workout");
+CREATE INDEX ON "events" ("location");
